c9e68542fb6900f166771920acf90e18e5e83f73
Ferenc Kagan <48235675+fka21@users.noreply.github.com>
2025-01-29 15:57:07 +0100

    Update sanger_processor.R

--- scripts/sanger_processor.R
+++ scripts/sanger_processor.R
@@ -1,14 +1,19 @@
-# Load necessary libraries
-library(sangeranalyseR)
-library(tidyverse)
-library(optparse)
+# Function to suppress library loading messages
+suppress_lib_loading <- function(package) {
+  suppressMessages(library(package, character.only = TRUE))
+}
+
+# Load necessary libraries without messages
+suppress_lib_loading("sangeranalyseR")
+suppress_lib_loading("tidyverse")
+suppress_lib_loading("optparse")
 
 # Define command line arguments
 option_list <- list(
   make_option(c("-d", "--directory"), type = "character", default = NULL, help = "Directory containing .ab1 files", metavar = "character"),
   make_option(c("-t", "--trim_method"), type = "character", default = "M2", help = "Trimming method (default is 'M2')", metavar = "character"),
-  make_option(c("-m1", "--m1_cutoff"), type = "numeric", default = NULL, help = "M1 Trimming Cutoff", metavar = "numeric"),
-  make_option(c("-m2", "--m2_cutoff_quality"), type = "numeric", default = 40, help = "M2 Cutoff Quality Score (default is 40)", metavar = "numeric"),
+  make_option(c("-m", "--m1_cutoff"), type = "numeric", default = NULL, help = "M1 Trimming Cutoff", metavar = "numeric"),
+  make_option(c("-c", "--m2_cutoff_quality"), type = "numeric", default = 40, help = "M2 Cutoff Quality Score (default is 40)", metavar = "numeric"),
   make_option(c("-w", "--m2_sliding_window_size"), type = "numeric", default = 5, help = "M2 Sliding Window Size (default is 5)", metavar = "numeric"),
   make_option(c("-b", "--base_num_per_row"), type = "numeric", default = 100, help = "Base number per row (default is 100)", metavar = "numeric"),
   make_option(c("-h", "--height_per_row"), type = "numeric", default = 200, help = "Height per row (default is 200)", metavar = "numeric"),
@@ -40,11 +45,11 @@ for (file in ab1_files) {
     geneticCode           = GENETIC_CODE,
     TrimmingMethod        = opt$trim_method,
     M1TrimmingCutoff      = opt$m1_cutoff,
-    M2CutoffQualityScore  = opt$m2_cutoff_quality,
-    M2SlidingWindowSize   = opt$m2_sliding_window_size,
-    baseNumPerRow         = opt$base_num_per_row,
-    heightPerRow          = opt$height_per_row,
-    signalRatioCutoff     = opt$signal_ratio_cutoff,
+    M2CutoffQualityScore  = opt$c,
+    M2SlidingWindowSize   = opt$w,
+    baseNumPerRow         = opt$b,
+    heightPerRow          = opt$h,
+    signalRatioCutoff     = opt$s,
     showTrimmed           = TRUE
   )
   
